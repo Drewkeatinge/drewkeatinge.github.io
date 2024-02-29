0003e629545823b20de87d763aa240c7f5101463
Andrew Keatinge <andrewgkeatinge@gmail.com>
2024-02-29 23:23:28 +1300

    links to blog posts added

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,24 @@
-// Function to handle button click
+// Function to handle when the button gets clicked
 function buttonClick(buttonId) {
-  // Close all dropdowns
-  var dropdowns = document.getElementsByClassName('dropdown-contents')
-  for (var i = 0; i < dropdowns.length; i++) {
-    var dropdown = dropdowns[i]
-    if (dropdown.classList.contains('show')) {
-      dropdown.classList.remove('show')
-    }
-  }
-
-  // Toggle the dropdown associated with the clicked button
+  // Creates a variable dropdownList which toggles the dropdown associated
+  // with the clicked button by combining the dropdownList ID with the buttonId
   var dropdownList = document.getElementById('dropdownList' + buttonId)
-  dropdownList.classList.toggle('show')
+
+  // This If statement checks if the dropdown is already visible via the class 'show'
+  if (dropdownList.classList.contains('show')) {
+    // If it's visible, hide it
+    dropdownList.classList.remove('show')
+  } else {
+    // If it's not visible, show it
+    dropdownList.classList.add('show')
+  }
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
+  // Check if the clicked element is not a dropdown button
   if (!event.target.matches('.drop-button')) {
+    // Close all dropdowns
     var dropdowns = document.getElementsByClassName('dropdown-contents')
     for (var i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i]
